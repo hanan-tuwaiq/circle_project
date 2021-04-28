972c9ae696d9bce81f180326509b290e9bed2dd0
Hanan <82478654+hanan-tuwaiq@users.noreply.github.com>
2021-04-28 23:26:11 +0300

    Add: Move & select

---
+++ circle_project/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace circle_project
+{
+    public partial class Form1 : Form
+    {
+        Pen pen = new Pen(Brushes.Black);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+
+        public int x = 300 , y = 300, width = 300, height = 300, deltaX, deltaY;
+        bool isActive = false, dragging = false, resize = false;
+        //active state handlers
+        Rectangle topRect, leftRect, bottomRect, rightRect, mainRect;
+
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(isActive && intersect(e))
+            {
+                var rec1 = new System.Drawing.Rectangle(e.X, e.Y, 10, 10);
+
+                if (rec1.IntersectsWith(topRect))
+                {
+                    this.resize = true;
+                    this.dragging = false;
+                }
+                else
+                {
+                    this.dragging = true;
+                    
+                }
+                deltaX = e.Location.X - this.x;
+                deltaY = e.Location.Y - this.y;
+            }
+        }
+  
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.dragging)
+            {
+                this.x = e.Location.X - deltaX;
+                this.y = e.Location.Y - deltaY;
+                this.Invalidate();
+            }
+            if (this.resize)
+            {
+                //(e.Location.Y - deltaY)
+                height = mainRect.Top - e.Location.Y;
+                Invalidate();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.dragging = false;
+            this.resize = false;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            pen.Width = 6.0f;//
+            mainRect = new Rectangle(x, y, width, height);
+            g.DrawEllipse(pen, mainRect);
+
+            if (isActive)
+            {
+                //mid circle
+                g.FillEllipse(redBrush, x + width / 2 - 10, y + height / 2 - 10, 20, 20);
+                //circles on the edges
+                //set the rectangles 
+                topRect = new Rectangle(x + width / 2 - 10, y - 10, 20, 20);
+                leftRect = new Rectangle(x - 10, y + height / 2 - 10, 20, 20);
+                bottomRect = new Rectangle(x + width / 2 - 10, y + height - 10, 20, 20);
+                rightRect = new Rectangle(x + width - 10, y + height / 2 - 10, 20, 20);
+
+                g.FillEllipse(redBrush, topRect);//upper edge 
+                g.FillEllipse(redBrush, leftRect);//left edge 
+                g.FillEllipse(redBrush, bottomRect);//bottom edge                                             
+                g.FillEllipse(redBrush, rightRect);//right edge     
+            }
+        }
+       public bool intersect(MouseEventArgs mouse)
+        {
+            var rec1 = new System.Drawing.Rectangle(this.x, this.y, this.width, this.height);
+            var rec2 = new System.Drawing.Rectangle(mouse.X, mouse.Y, 10, 10);
+            return rec1.IntersectsWith(rec2);
+        }
+       
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (intersect(e))
+            {
+                this.isActive = true;
+                this.Invalidate();
+            }
+            else
+            {
+                this.isActive = false;
+                this.Invalidate();
+            }
+          
+        }
+       
+    }
+}
